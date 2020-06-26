dcafca9fff897f8fa7fd4687090797d0cc28189a
james wu <jwu_nyc@yahoo.com>
2020-06-26 22:15:47 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,7 +1,10 @@
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
-  if string[0] == "s" ? "ssssssssss" + string string
+  if string[0] == "s"
+    "ssssssssss" + string
+  else
+    string
   end
 end
 
